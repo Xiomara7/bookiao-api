3388cf875be7dda84850cf9bb0d110b0614e3e31
Christian <christian.etpr10@gmail.com>
2014-10-28 19:14:15 -0400

    Setting everything up for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bookiao.wsgi --log-file -
--- bookiao/settings.py
+++ bookiao/settings.py
@@ -99,3 +99,23 @@ REST_FRAMEWORK = {
   ),
   'PAGINATE_BY': 10
 }
+
+if os.getenv('PRODUCTION') == "True":
+  # Parse database configuration from $DATABASE_URL
+  import dj_database_url
+  DATABASES['default'] =  dj_database_url.config()
+
+  # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+  SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+  # Allow all host headers
+  ALLOWED_HOSTS = ['*']
+
+  # Static asset configuration
+  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+  STATIC_ROOT = 'staticfiles'
+  STATIC_URL = '/static/'
+
+  STATICFILES_DIRS = (
+      os.path.join(BASE_DIR, 'static'),
+  )
